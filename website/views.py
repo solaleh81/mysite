ba219afe60aabda0f505b9506dae3cfe7462af6f
solaleh81 <solalehalizadeh81.com>
2023-12-29 15:50:12 +0330

    add: def http_test & def json_test.

--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+def http_test(request):
+    return HttpResponse('http-test')
+
+def json_test(request):
+    return JsonResponse({'name': 'solale'})
