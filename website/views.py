f21e686a989188d707e06646ece19c63db003af8
solaleh81 <solalehalizadeh81.com>
2023-12-29 15:59:41 +0330

    add: index_view & about_view & contact_view.

--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-def http_test(request):
-    return HttpResponse('http-test')
+def index_view(request):
+    return HttpResponse('<h1>Home Page</h1>')
 
-def json_test(request):
-    return JsonResponse({'name': 'solale'})
+def about_view(request):
+    return HttpResponse('<h1>About Page</h1>')
+
+def contact_view(request):
+    return HttpResponse('<h1>Contact Page</h1>')
+
