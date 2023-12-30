7039737aafb9a3114d7f5d92ef40d4479afb0206
solaleh81 <solalehalizadeh81.com>
2023-12-30 13:59:46 +0330

    add: media url.

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [
+urlpatterns = ([
     path('admin/', admin.site.urls),
     path('website/', include('website.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+])
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
