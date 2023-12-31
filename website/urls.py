682e20252361de476ad6fd3f08c94382e6eeb5ff
solaleh81 <solalehalizadeh81.com>
2023-12-31 10:11:42 +0330

    add: test url.

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
+app_name = 'website'
 urlpatterns = [
     path('', views.index_view, name='home'),
     path('about', views.about_view, name='about'),
     path('contact', views.contact_view, name='contact'),
+    path('test', views.test_view, name='test'),
 
 ]
