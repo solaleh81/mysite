d3b9a504ba11d1048c4ee21971d7cff8c96e9eb5
solaleh81 <solalehalizadeh81.com>
2024-01-02 21:21:21 +0330

    add: def str.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    # image
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    #tag
+    #category
+    counted_views =  models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.id}"
