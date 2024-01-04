f73739f932f745c5724c746cb8acfeec527372c5
solaleh81 <solalehalizadeh81.com>
2024-01-04 21:47:45 +0330

    add: id to single url.

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.blog_view, name='blog'),
-    path('single', views.blog_single, name='single'),
+    path('single/<int:id>', views.blog_single, name='single'),
     path('test', views.test, name='test'),
 
 ]
