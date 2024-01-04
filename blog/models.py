860ad25f74129c3a2736f5cff2c07712c07170c1
solaleh81 <solalehalizadeh81.com>
2024-01-04 18:14:04 +0330

    refactor: rename created field.

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Post(models.Model):
     # image
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(null=True)
     #tag
     #category
-    counted_views =  models.IntegerField(default=0)
+    counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
-    created = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
