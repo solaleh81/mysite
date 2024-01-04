f649e94f83463c9665a8b88564e7976a75c0597b
solaleh81 <solalehalizadeh81.com>
2024-01-04 22:14:09 +0330

    add: author field to Post class.

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     # image
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=100)
     content = models.TextField(null=True)
     #tag
