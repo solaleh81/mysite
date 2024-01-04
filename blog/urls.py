24b6907bf3ae8e52cd09080cf00b41a8616705b7
solaleh81 <solalehalizadeh81.com>
2024-01-04 19:09:56 +0330

    add: test url.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.blog_view, name='blog'),
     path('single', views.blog_single, name='single'),
+    path('test', views.test, name='test'),
 
 ]
