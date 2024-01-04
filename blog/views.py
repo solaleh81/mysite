99a19972db0a932570e7dcad310b3856bb1dfea3
solaleh81 <solalehalizadeh81.com>
2024-01-04 21:48:14 +0330

    add: context to single view.

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,9 @@ def blog_view(request):
     posts = Post.objects.filter(status=0)
     return render(request, 'blog/blog-home.html', context={'posts': posts})
 
-def blog_single(request):
-    context = {'title': 'bitcoin crashed again!', 'content': 'bitcoin was flying...'}
+def blog_single(request, id):
+    post = Post.objects.get(pk=id)
+    context = {'post': post}
     return render(request, 'blog/blog-single.html', context=context)
 
 def test(request):
