7e944361ee56003f16d7f6b59b5d51b3dd404fe6
solaleh81 <solalehalizadeh81.com>
2024-01-04 18:12:53 +0330

    Add: PostAdmin class.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = "-empty-"
+    # fields = ('title',)
+    list_display = ["title", "content", "counted_views", "created_date", "status"]
+    list_filter = ("status",)
+    ordering = ['created_date']
+    search_fields = ['title', 'content']
+# admin.site.register(Post)
