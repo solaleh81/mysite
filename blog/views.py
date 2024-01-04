f628846cb021e6c7abb3879ba9d2a713858ea33f
solaleh81 <solalehalizadeh81.com>
2024-01-04 19:10:42 +0330

    add: test def.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from blog.models import Post
 def blog_view(request):
     return render(request, 'blog/blog-home.html')
 
@@ -7,3 +7,6 @@ def blog_single(request):
     context = {'title': 'bitcoin crashed again!', 'content': 'bitcoin was flying...'}
     return render(request, 'blog/blog-single.html', context=context)
 
+def test(request):
+    posts = Post.objects.filter(status=0)
+    return render(request, 'test.html', context={'posts': posts})
