af3d9c4b643be1429d3f3824b7be875d5af72a84
solaleh81 <solalehalizadeh81.com>
2024-01-05 16:49:42 +0330

    add: category url.

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.blog_view, name='blog'),
     path('single/<int:id>', views.blog_single, name='single'),
+    path('category/<str:cat_name>', views.blog_category, name='category'),
     path('test', views.test, name='test'),
 
 ]
