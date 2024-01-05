b5fac992171519a77f3e58e2a907e111f02b7ed1
solaleh81 <solalehalizadeh81.com>
2024-01-05 16:50:38 +0330

    add: def postcategories().

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -22,3 +22,13 @@ def snippet(value):
 def latestposts():
     posts = Post.objects.filter(status=0).order_by("created_date")
     return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts = Post.objects.filter(status=0)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+
+    return {'categories': cat_dict}
