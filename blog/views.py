d3b088fe8fc9c427b4b50212a8c83ae64f23d3c3
solaleh81 <solalehalizadeh81.com>
2024-01-05 17:39:00 +0330

    add: kwrags for blog_view.

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
-def blog_view(request):
+
+
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=0)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     return render(request, 'blog/blog-home.html', context={'posts': posts})
+
 
 def blog_single(request, id):
     post = get_object_or_404(Post, id=id)
     context = {'post': post}
     return render(request, 'blog/blog-single.html', context=context)
 
+
 def test(request):
     posts = Post.objects.filter(status=0)
     return render(request, 'test.html', context={'posts': posts})
+
 
 def blog_category(request, cat_name):
     posts = Post.objects.filter(status=0)
