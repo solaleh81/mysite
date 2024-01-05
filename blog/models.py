7c7e492df6a4e72fa3ebbd82c298684f293735f9
solaleh81 <solalehalizadeh81.com>
2024-01-05 12:13:12 +0330

    add: Category table.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
@@ -8,7 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(null=True)
     #tag
-    #category
+    category =  models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
@@ -21,3 +26,4 @@ class Post(models.Model):
         # verbose_name_plural = 'پست ها'
     def __str__(self):
         return f"{self.title} - {self.id}"
+
