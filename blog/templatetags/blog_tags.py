8f5da4eb436de5fa8c02fcce5a402e3d545ff1d8
solaleh81 <solalehalizadeh81.com>
2024-01-05 15:17:19 +0330

    add: latestposts.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -6,10 +6,19 @@ register = template.Library()
 
 @register.simple_tag(name='totalposts')
 def function():
-    posts = Post.objects.all().count()
+    posts = Post.objects.filter(status=1).count()
+    print(posts)
     return posts
 
 @register.simple_tag(name='posts')
 def function():
-    posts = Post.objects.all().count()
+    posts = Post.objects.filter(status=1)
     return posts
+
+@register.filter
+def snippet(value):
+    return value[:100]
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def latestposts():
+    posts = Post.objects.filter(status=0).order_by("created_date")
+    return {'posts': posts}
