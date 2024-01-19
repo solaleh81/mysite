192ce86a371cc064258321b26ec4208b89993141
solaleh81 <solalehalizadeh81.com>
2024-01-19 17:38:46 +0330

    add: class NewsLetter.

--- website/models.py
+++ website/models.py
@@ -13,3 +13,9 @@ class Contact(models.Model):
         ordering = ['created_date']
     def __str__(self):
         return self.name
+
+class NewsLetter(models.Model):
+        email = models.EmailField()
+
+        def __str__(self):
+            return self.email
