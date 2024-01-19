d6301aa9277674ad46b17c80e87791b8d20812d2
solaleh81 <solalehalizadeh81.com>
2024-01-19 20:04:20 +0330

    add: accounts url.

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = ([
     path('admin/', admin.site.urls),
     path('website/', include('website.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
+
 ])
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
