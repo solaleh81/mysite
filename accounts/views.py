b9972e7a42a7183f8bbd9a3cc730fd6f2cc28cf2
solaleh81 <solalehalizadeh81.com>
2024-01-26 12:19:33 +0330

    add: Authentication form for def login_view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,25 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('website:home')
-    return render(request, 'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('blog:blog')
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', context={'form': form})
+    else:
+        return redirect('blog:blog')
+
+
 
 
 def logout_view(request):
